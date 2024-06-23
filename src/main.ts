da2dd03e1e8136b3309e100ae4b0b1cd8d05a5e7
MikroTik2 <111369356+MikroTik2@users.noreply.github.com>
2024-06-24 01:51:00 +0300

    change: #7 config telegram

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const bot = await app.get(TelegramService);
 
-    app.use('/webhook') {
+    app.use('/webhook', () => {
         const res = bot.telegram.getWebhookInfo();
         console.log(res);
-    }
+    });
 
     await app.listen(process.env.PORT || 3000);
 }
