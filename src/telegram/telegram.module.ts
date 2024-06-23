3e31cfe599edc3328964d856e24ffea7d7b2c07d
MikroTik2 <111369356+MikroTik2@users.noreply.github.com>
2024-06-24 00:19:28 +0300

    change: #6 config telegram

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,15 +3,13 @@ import { TelegrafModule } from "nestjs-telegraf";
 import { TelegramService } from "@/telegram/telegram.service";
 import { options } from "@/telegram/telegram-config.factory";
 import { DownloadModule } from "@/download/download.module";
-import { TelegramController } from "@/telegram/telegram.controller";
 
 @Module({
             imports: [
                         TelegrafModule.forRootAsync(options()),
-
                         DownloadModule,
             ],
-            controllers: [TelegramController],
+            
             providers: [TelegramService],
 })
 
