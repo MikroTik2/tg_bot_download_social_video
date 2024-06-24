f89bb03b127e984d6673c2967021c8ebfe5067cc
MikroTik2 <111369356+MikroTik2@users.noreply.github.com>
2024-06-24 07:15:52 +0300

    change: #7 config telegram

--- src/download/download.service.ts
+++ src/download/download.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { catchError, map } from 'rxjs';
 import { lastValueFrom } from 'rxjs';
-import ytdl from 'ytdl-core';
+// import ytdl from 'ytdl-core';
 
 @Injectable()
 export class DownloadService {
@@ -17,7 +17,7 @@ export class DownloadService {
     ) {};
             
     async downloadYouTube(url: string) {
-        if (url.startsWith('https://www.youtube.com/')) {
+        // if (url.startsWith('https://www.youtube.com/')) {
 
             // const info = await ytdl.getInfo(url);
             // const stream = ytdl(url, { filter: "audio", quality: "highestvideo" })
@@ -28,9 +28,9 @@ export class DownloadService {
             //     author: info.videoDetails.author
             // };
 
-        } else {
-            return { error: 'Неверный URL: ' + url };
-        };
+        // } else {
+        //     return { error: 'Неверный URL: ' + url };
+        // };
     };
 
     async downloadInstagram(url: string) {
